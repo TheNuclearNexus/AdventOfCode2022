35cb355f10c1dffc5a0ce367da4cc008a17662ca
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-12-01 14:48:36 -0500

    cleaned up significantly

--- Day 1/index.ts
+++ Day 1/index.ts
@@ -12,22 +12,13 @@ async function main() {
     const rawData = fs.readFileSync('input.txt', { encoding: 'utf8' })
 
     // Select all elf groupings of calories
-    const elves = rawData.match(/([0-9]+\n)+\n/g)?.map(
-        r => r.split('\n')
-            // Convert numbers to Number type
-            .map(n => Number.parseInt(n))
-            // Cull remaining whitespace from duplicate \n
-            .slice(0, -2)
-    )
+    const elves = rawData.split('\n\n').map(e => sum(e.split('\n').map(n => n !== '' ? Number.parseInt(n) : 0)))
     if (elves === undefined)
         return
-    elves.sort((a, b) => sum(a) - sum(b))
 
-    console.log('Part 1:', sum(elves[elves.length - 1]))
-
+    console.log('Part 1:', elves[elves.length - 1])
     const topThree = elves.splice(elves.length - 3, 3)
-
-    console.log('Part 2: ', sum(topThree.map(e => sum(e))))
+    console.log('Part 2: ', sum(topThree))
 }
 
 main()
