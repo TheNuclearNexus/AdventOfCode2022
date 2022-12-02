74049fe3b3928a0a07b6ac9c8c77426fe75b8526
TheNuclearNexus <thenuclearnexus@gmail.com>
2022-12-02 18:06:51 -0500

    fixed say 1

--- Day 1/index.ts
+++ Day 1/index.ts
@@ -1,24 +1,14 @@
 // Day 1 Index
 import * as fs from 'fs'
 
-function sum(elf: number[]) {
-    let sum = 0;
-    elf.forEach((e) => sum += e)
-    return sum
-}
-
 async function main() {
     // Load Data
     const rawData = fs.readFileSync('input.txt', { encoding: 'utf8' })
 
     // Select all elf groupings of calories
-    const elves = rawData.split('\n\n').map(e => sum(e.split('\n').map(n => n !== '' ? Number.parseInt(n) : 0)))
-    if (elves === undefined)
-        return
-
+    const elves = rawData.split('\n\n').map(e => e.split('\n').map(i => i !== '' ? Number.parseInt(i) : 0).reduce((a,b)=> a+b)).sort((a,b) => a-b)
     console.log('Part 1:', elves[elves.length - 1])
-    const topThree = elves.splice(elves.length - 3, 3)
-    console.log('Part 2: ', sum(topThree))
+    console.log('Part 2:', elves.splice(elves.length - 3, 3).reduce((a,b)=>a+b))
 }
 
 main()
